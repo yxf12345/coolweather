3c1de294932af1909d790b1b44761ff0ebe7b77f
yxf <2563239715@qq.com>
2018-05-29 22:31:45 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/example/a15978/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.a15978.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 15978 on 2018/5/29.
+ */
+
+public class Province extends DataSupport
+{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
